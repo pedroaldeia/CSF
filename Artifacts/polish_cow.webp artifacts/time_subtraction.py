27071754e1c8f8523ad7552dcf8bbb4786dbcffd
Pedro Aldeia <pedroaldeia@tecnico.ulisboa.pt>
2026-09-21 23:27:51 +0100

    polish cow artifacts cleanup

--- Artifacts/polish_cow.webp artifacts/time_subtraction.py
+++ Artifacts/polish_cow.webp artifacts/time_subtraction.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b974ed0c563f0d1fe1e340e415cbe549feedc623df43ff05b3fa30cd8e8a920f
-size 5483
+oid sha256:9a10e00e9ea97a90aaeea24f1329878e3e6e7f15d3ae255197179dda54ff1527
+size 1589
