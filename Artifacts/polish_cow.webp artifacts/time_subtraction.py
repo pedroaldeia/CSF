565f3fcecd1984f2058b3143040fbbc05ce03d97
Vladislav Nagornii <vladislav.nagornii@tecnico.ulisboa.pt>
2026-09-23 18:08:10 +0100

    report

--- Artifacts/polish_cow.webp artifacts/time_subtraction.py
+++ Artifacts/polish_cow.webp artifacts/time_subtraction.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a10e00e9ea97a90aaeea24f1329878e3e6e7f15d3ae255197179dda54ff1527
-size 1589
+oid sha256:d601dccb8e1f55afe3e6abbac80d6677e6c3281a2f3c4429ae404fdf7595e666
+size 1600
